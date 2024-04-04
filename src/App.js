b576f0a5d92231fd9a566f6a354e2d48860fc17c
arielperi <arikpre@gmail.com>
2024-04-04 15:20:05 +0300

    change home screen

--- src/App.js
+++ src/App.js
@@ -15,7 +15,7 @@ function App() {
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React!!
+          Salut Yvo, comment ça va?
         </a>
       </header>
     </div>
